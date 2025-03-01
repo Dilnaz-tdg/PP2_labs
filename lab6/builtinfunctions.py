b6e7364513ab7c19364945baa4cf2ff23d557d9d
Dilnaz Tubekbayeva <dilnaz_tubekbaeva@mail.ru>
2025-03-02 01:17:40 +0600

    new

---
+++ lab6/builtinfunctions.py
@@ -0,0 +1,41 @@
+#1
+
+lst = [1,2,3,4,5,6]
+answ = 1
+for i in range(1, len(lst)+1):
+    answ = answ * i
+
+print(answ)
+
+#2
+userstr = input()
+uprc = 0
+lwc = 0
+for i in userstr:
+    if(ord(i) >= 65 and ord(i) <= 90):
+        uprc += 1
+    elif(ord(i) >= 97 and ord(i) <= 122):
+        lwc += 1
+
+print(f"upper case: {uprc}")
+print(f"lower case: {lwc}")
+
+#3 
+userword = input()
+if userword == ''.join(reversed(userword)): 
+     print(f"{userword} is palindrome")
+
+#4
+import time
+import math
+def invoke():
+    num = int(input())
+    timeusers = int(input())
+    time.sleep(timeusers / 1000)
+    print(f"Square root of {num} after {timeusers} miliseconds is {math.sqrt(num)}")
+
+invoke()
+
+#5
+tuple = (1, 1, "yes", "no")
+print(all(tuple))
